a07b1cae2ea948ed59b7cac4359c191bcec6c8ff
cmarin001 <cmarin6051@gmail.com>
2024-11-25 11:13:52 -0500

    fix: remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import { enviroment } from "./config/environment";
 
-// import { AppContextProvider } from "./context/AppContext";
-// import { usePortalData } from "./hooks/usePortalData";
-// import { useBusinessManagers } from "./hooks/useBusinessManagers";
-// import { useAuthRedirect } from "./hooks/useAuthRedirect";
-// import { ErrorPage } from "./components/layout/ErrorPage";
+import { AppContextProvider } from "./context/AppContext";
+import { usePortalData } from "./hooks/usePortalData";
+import { useBusinessManagers } from "./hooks/useBusinessManagers";
+import { useAuthRedirect } from "./hooks/useAuthRedirect";
+import { ErrorPage } from "./components/layout/ErrorPage";
 
 function LogOut() {
   localStorage.clear();
@@ -25,48 +25,50 @@ function LogOut() {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="*" element={<h1>Home credicar</h1>} 
-            errorElement={<div> Error credicar dasdsdasdd</div >} />
-      {/* errorElement={<ErrorPage />} /> */}
+      <Route
+        path="*"
+        element={<h1>Home credicar</h1>}
+        errorElement={<ErrorPage />}
+      />
 
       <Route path="logout" element={<LogOut />} />
     </>,
   ),
 );
-// const url = new URL(window.location.href);
-// const params = new URLSearchParams(url.search);
-// const portalCode = params.get("portal");
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+const portalCode = params.get("portal");
 
 function App() {
-  // const { portalData, hasError: portalError } = usePortalData(portalCode);
-  // const { businessManagersData, hasError: businessError } =
-  //   useBusinessManagers(portalData);
+  const { portalData, hasError: portalError } = usePortalData(portalCode);
+  const { businessManagersData, hasError: businessError } =
+    useBusinessManagers(portalData);
 
-  // const {
-  //   hasError: authError,
-  //   isLoading,
-  //   isAuthenticated,
-  // } = useAuthRedirect(portalData, businessManagersData, portalCode);
+  const {
+    hasError: authError,
+    isLoading,
+    isAuthenticated,
+  } = useAuthRedirect(portalData, businessManagersData, portalCode);
 
-  // const hasError = portalError || businessError || authError;
+  const hasError = portalError || businessError || authError;
 
-  // if (isLoading) {
-  //   return null;
-  // }
+  if (isLoading) {
+    return null;
+  }
 
-  // if (hasError && !isAuthenticated) {
-  //   return <ErrorPage />;
-  // }
+  if (hasError && !isAuthenticated) {
+    return <ErrorPage />;
+  }
 
-  // if (!isAuthenticated) {
-  //   return null;
-  // }
+  if (!isAuthenticated) {
+    return null;
+  }
 
   return (
     <>
-      {/* <AppContextProvider> */}
+      <AppContextProvider>
         <RouterProvider router={router} />
-      {/* </AppContextProvider> */}
+      </AppContextProvider>
     </>
   );
 }
