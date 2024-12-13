add4d14e025566b3e8753e3389dd2fe6531b46f2
angie pinilla <angiepinillanova@gmail.com>
2024-12-13 13:39:01 -0500

    fix: adjust details destination modal component

--- src/components/modals/DetailsDestinationModal/CreditLineTab/index.tsx
+++ src/components/modals/DetailsDestinationModal/CreditLineTab/index.tsx
@@ -1,8 +1,11 @@
-import { IRuleDecision } from "@isettingkit/input";
+import {
+  IRuleDecision,
+  ValueDataType,
+  ValueHowToSetUp,
+} from "@isettingkit/input";
+import { BusinessRules } from "@isettingkit/business-rules";
 import { Stack } from "@inubekit/stack";
 import { Divider } from "@inubekit/divider";
-
-import { CreditLineCard } from "@components/feedback/CreditLineCard";
 import { tokens } from "@design/tokens";
 import { StyledContainer } from "./styles";
 
@@ -10,15 +13,58 @@ interface ICreditLineTab {
   data: IRuleDecision[];
 }
 
+const decisionTemplate: IRuleDecision = {
+  name: "TasaEfectivaAnual",
+  dataType: ValueDataType.PERCENTAGE,
+  value: { from: -1, to: -1 },
+  valueUse: ValueHowToSetUp.RANGE,
+  startDate: "",
+  endDate: "",
+  conditions: [
+    {
+      name: "AntigüedadDelCliente(Días)",
+      dataType: ValueDataType.ALPHABETICAL,
+      value: "",
+      valueUse: ValueHowToSetUp.EQUAL,
+    },
+  ],
+};
+
 function CreditLineTab(props: ICreditLineTab) {
   const { data } = props;
 
   return (
     <Stack direction="column" gap={tokens.spacing.s300}>
       <StyledContainer>
-        {data.map((decision, index) => (
-          <CreditLineCard data={decision} key={index} />
-        ))}
+        <BusinessRules
+          controls={false}
+          decisions={data}
+          textValues={{
+            selectOptions: "Seleccione las opciones",
+            selectOption: "Seleccione una opción",
+            rangeMin: (label: string) => `${label} Minima`,
+            rangeMax: (label: string) => `${label} Maxima`,
+            reasonForChange: "",
+            change: "",
+            changePlaceholder: "",
+            termStart: "",
+            termEnd: "",
+            cancel: "",
+            confirm: "",
+            none: "Ninguno",
+            factsThatConditionIt: "Condiciones que lo determinan",
+            criteria: "Criterios",
+            terms: "Vigencia",
+          }}
+          decisionTemplate={decisionTemplate}
+          isModalOpen={false}
+          selectedDecision={null}
+          loading={false}
+          handleOpenModal={() => console.log("")}
+          handleCloseModal={() => console.log("")}
+          handleSubmitForm={() => console.log("")}
+          handleDelete={() => console.log("")}
+        />
       </StyledContainer>
       <Divider />
     </Stack>
